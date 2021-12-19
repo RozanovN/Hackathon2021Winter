b3d0914fa84c351abdae5e41629e3a10ac097136
Jimmy Wong <cy.jimmy.wong@gmail.com>
2021-12-18 17:11:30 -0800

    resolve git conflict

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
->>>>>>> 799ae417783a5557f3e1fbb8b9ba6519b2779630
 const firebaseConfig = {
   apiKey: "AIzaSyACt8rkK19iFkJLs3AJOxNVrQQ52JIhG98",
   authDomain: "winter2021hacks.firebaseapp.com",
@@ -16,10 +13,5 @@ const firebaseConfig = {
   appId: "1:443803406838:web:1d35e3c8ad4d43ebdc37d7"
 };
 
-<<<<<<< HEAD
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-=======
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
->>>>>>> 799ae417783a5557f3e1fbb8b9ba6519b2779630
