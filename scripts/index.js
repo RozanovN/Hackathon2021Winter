fe265f4dee00ddf1f3dbc5edb1adc9c540587d4a
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2021-12-18 21:14:20 -0800

    added country flag

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,99 @@
-var xhr = new XMLHttpRequest();
+function populate() {
+    var xhr = new XMLHttpRequest();
 
-xhr.withCredentials = true;
+    xhr.withCredentials = true;
 
-xhr.onload = function () {
-    var data = JSON.parse(this.response);
-    console.log(data.response);
+    xhr.onload = function () {
 
-    data.response.sort(function(a, b){
-        if (a.country.toUpperCase() < b.country.toUpperCase()){
-            return -1;
-        }
-        if (a.country.toUpperCase() > b.country.toUpperCase()){
-            return 1;
-        }
-        return 0;
+        var data = JSON.parse(this.response);
+
+        data.response.sort(function (a, b) {
+            if (a.country.toUpperCase() < b.country.toUpperCase()) {
+                return -1;
+            }
+            if (a.country.toUpperCase() > b.country.toUpperCase()) {
+                return 1;
+            }
+            return 0;
+        })
+
+        data.response.forEach(country => {
+            countryName = country.country;
+            activeCases = country.cases.active;
+            newCases = country.cases.new;
+            if (newCases == null) {
+                newCases = "0"
+            }
+            if (activeCases == null) {
+                activeCases = "0"
+            }
+
+            card = document.createElement("div")
+            card.setAttribute("class", "card")
+
+            countryImage = document.createElement("img")
+            countryImage.setAttribute("class", "country-img")
+            countryImage.setAttribute("data-country", countryName)
+            countryImage.setAttribute("src", "https://dummyimage.com/600x400/000/fff")
+            card.appendChild(countryImage)
+
+            cardDescription = document.createElement("div")
+            cardDescription.setAttribute("class", "card-description")
+
+            countryTitle = document.createElement("div")
+            countryTitle.setAttribute("class", "country-name")
+            countryTitle.innerHTML = countryName
+
+            countryNewCases = document.createElement("div")
+            countryNewCases.setAttribute("class", "country-new-cases")
+            countryNewCases.innerHTML = `New Cases: ${newCases}`
+
+            countryActiveCases = document.createElement("div")
+            countryActiveCases.setAttribute("class", "country-active-cases")
+            countryActiveCases.innerHTML = `Active Cases: ${activeCases}`
+
+            cardDescription.appendChild(countryTitle)
+            cardDescription.appendChild(countryNewCases)
+            cardDescription.appendChild(countryActiveCases)
+
+            card.appendChild(cardDescription)
+            card.setAttribute("data-newcase", newCases)
+            card.setAttribute("data-country", countryName)
+
+            section = document.querySelector("section")
+            section.appendChild(card)
+
+        });
+
+        getFlag()
+    };
+
+    xhr.open("GET", "https://covid-193.p.rapidapi.com/statistics");
+    xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "f7328ad34dmsh89abcc0730b26ddp1b9d14jsnb831f0b29bce");
+
+    xhr.send();
+}
+
+function getFlag() {
+    countriesImg = document.querySelectorAll(".country-img")
+    countriesImg.forEach(img => {
+        countryName = img.dataset.country
+        xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            data = JSON.parse(this.response);
+            try {
+                img.setAttribute("src", data[0].flags.png)
+            }
+            catch {
+            }
+        };
+        xhr.open("GET", `https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
+        xhr.send();
     })
+}
 
-    data.response.forEach(country => {
-        countryName = country.country;
-        activeCases = country.cases.active;
-        newCases = country.cases.new;
-        if (newCases == null) {
-            newCases = "0"
-        }
-        if (activeCases == null) {
-            activeCases = "0"
-        }
-
-        card = document.createElement("div")
-        card.setAttribute("class", "card")
-
-        countryImage = document.createElement("img")
-        countryImage.setAttribute("class", "news-img")
-        countryImage.setAttribute("src", "https://dummyimage.com/600x400/000/fff")
-        card.appendChild(countryImage)
-
-        cardDescription = document.createElement("div")
-        cardDescription.setAttribute("class", "card-description")
-
-        countryTitle = document.createElement("div")
-        countryTitle.setAttribute("class", "country-name")
-        countryTitle.innerHTML = countryName
-
-        countryNewCases = document.createElement("div")
-        countryNewCases.setAttribute("class", "country-new-cases")
-        countryNewCases.innerHTML = `New Cases: ${newCases}`
-
-        countryActiveCases = document.createElement("div")
-        countryActiveCases.setAttribute("class", "country-active-cases")
-        countryActiveCases.innerHTML = `Active Cases: ${activeCases}`
-
-        cardDescription.appendChild(countryTitle)
-        cardDescription.appendChild(countryNewCases)
-        cardDescription.appendChild(countryActiveCases)
-
-        card.appendChild(cardDescription)
-        card.setAttribute("data-newcase", newCases)
-        card.setAttribute("data-country", countryName)
-
-        section = document.querySelector("section")
-        section.appendChild(card)
-    });
-};
-
-xhr.open("GET", "https://covid-193.p.rapidapi.com/statistics");
-xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "f7328ad34dmsh89abcc0730b26ddp1b9d14jsnb831f0b29bce");
-
-xhr.send();
-
-
+populate()
 
 // READ data from CSV and WRITE to database
 async function getCSVdata() {
@@ -121,7 +145,7 @@ function sortCaseAsc() {
         }
         return newCase1 - newCase2
     })
-   
+
     let section = document.querySelector("section")
     let sortedCards = []
     for (let i = 0; i < itemList.length; i++) {
@@ -162,7 +186,7 @@ function sortCaseDes() {
         }
         return newCase2 - newCase1
     })
-   
+
     let section = document.querySelector("section")
     let sortedCards = []
     for (let i = 0; i < itemList.length; i++) {
@@ -189,15 +213,15 @@ function sortAlpha() {
     })
 
     itemList.sort(function (a, b) {
-        if (a.dataset.country.toUpperCase() < b.dataset.country.toUpperCase()){
+        if (a.dataset.country.toUpperCase() < b.dataset.country.toUpperCase()) {
             return -1;
         }
-        if (a.dataset.country.toUpperCase() > b.dataset.country.toUpperCase()){
+        if (a.dataset.country.toUpperCase() > b.dataset.country.toUpperCase()) {
             return 1;
         }
         return 0;
     })
-   
+
     let section = document.querySelector("section")
     let sortedCards = []
     for (let i = 0; i < itemList.length; i++) {
@@ -219,13 +243,13 @@ document.querySelector("#sort-icon").addEventListener("click", sortOption)
 
 function sortOption() {
     current = document.querySelector("#sort-option").innerHTML
-    if (current == "Alphabatical order"){
+    if (current == "Alphabatical order") {
         sortCaseAsc()
     }
-    if (current == "Least new case"){
+    if (current == "Least new case") {
         sortCaseDes()
     }
-    if (current == "Most new case"){
+    if (current == "Most new case") {
         sortAlpha()
     }
 }
